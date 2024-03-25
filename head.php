127166af97646d6b46481e51cde8ee964e3aa4f7
Griffin <123077668+GriffinMulei@users.noreply.github.com>
2024-03-25 13:05:04 +0300

    Update head.php

--- head.php
+++ head.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
-<title>Home | Jewelery for u</title>
-<link rel="icon" type="image/png" href="pics/jw.png"/>
-<link href="css/css.css" rel="Stylesheet" type="text/css" />
-<div id="head">
-<img src="pics/jw.jpg" alt="Mountain View" >
-<h1>
-<marquee width="50%">Jewelery for u</marquee></h1>
-</div>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | Jewelry for You</title>
+    <link rel="icon" type="image/png" href="pics/jw.png"/>
+    <link href="css/css.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
-<hr class="hr">
-<nav id="nav"> 
-<ul>
-<li><a href="home.php">Home</a></li>
-<li><a href="contacts.php">Contacts</a></li>
-<li><a href="../about/index.html">Help</a></li>
-<li><A HREF="login.php" onClick="return targetopener(this,true,true)">Sign up | Login</A></li>
-<li class="member">Hi</a></li>
-</ul>
-</nav>
+    <div id="head">
+        <img src="pics/jw.jpg" alt="Jewelry for You">
+        <h1>Jewelry for You</h1>
+    </div>
+    
+    <hr class="hr">
 
-<hr class="hr">
+    <nav id="nav"> 
+        <ul>
+            <li><a href="home.php">Home</a></li>
+            <li><a href="contacts.php">Contacts</a></li>
+            <li><a href="../about/index.html">Help</a></li>
+            <li><a href="login.php" onClick="return targetopener(this,true,true)">Sign up | Login</a></li>
+            <li class="member">Hi</li>
+        </ul>
+    </nav>
 
+    <hr class="hr">
 </body>
-
 </html>
