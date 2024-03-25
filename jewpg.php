b9e97446d70bd55abfc175cc0c0a523c4ceb9259
Griffin <123077668+GriffinMulei@users.noreply.github.com>
2024-03-25 12:46:34 +0300

    Update jewpg.php

--- jewpg.php
+++ jewpg.php
@@ -1,20 +1,26 @@
+<!DOCTYPE html>
 <html>
 <head>
-<title>Watches | Jewelery for u</title>
-<link href="css/css.css" rel="Stylesheet" type="text/css" />
-<?php include_once("head.php");?>
-</head><?php include_once("jewelry.php");?>
+    <title>Watches | Jewelry for You</title>
+    <link href="css/css.css" rel="stylesheet" type="text/css" />
+    <?php include_once("head.php"); ?>
 </head>
 <body>
-<div id="video">
-<video width="800" height="300" autoplay>
-  <source src="pics/video/jew.mp4" type="video/mp4">
-</video>
-</div>
-<div id="page">
+    <?php include_once("jewelry.php"); ?>
 
-</div>
-<footer>
-<?php include_once("footer.php");?>
-</footer>
+    <div id="video">
+        <video width="800" height="300" autoplay muted loop>
+            <source src="pics/video/jew.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+    </div>
+
+    <div id="page">
+        <!-- Page content goes here -->
+    </div>
+
+    <footer>
+        <?php include_once("footer.php"); ?>
+    </footer>
+</body>
 </html>
