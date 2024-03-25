03b07f133453ccbf9bafacc223bec0e33595f68f
Griffin <123077668+GriffinMulei@users.noreply.github.com>
2024-03-25 12:59:10 +0300

    Update home.php

--- home.php
+++ home.php
@@ -1,27 +1,53 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<link href="css/css.css" rel="Stylesheet" type="text/css" />
-<link href="css/pages.css" rel="Stylesheet" type="text/css" />
-<?php include_once("head.php");?>
-</head><?php include_once("jewelry.php");?>
-<body >
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jewelry Collection</title>
+    <link href="css/css.css" rel="stylesheet" type="text/css">
+    <link href="css/pages.css" rel="stylesheet" type="text/css">
+    <?php include_once("head.php"); ?>
+</head>
+<body>
+    <?php include_once("jewelry.php"); ?>
 
-<div id="body">
-<table >
-<tr>
-<td><a href="Ankletes/Ankletes.php"><img src="pics/anklets/k14530991.jpg" alt="Mountain View" ></a><p class="jew">Anklets</p></td>
-<td><a href=""><img src="pics/bracelets/diamond-bracelets-7899035.jpg" alt="Mountain View" ></a><p class="jew">Bracelets</p></td>
-<td><a href=""><img src="pics/earings/k16417032.jpg" alt="Mountain View" ></a><p class="jew">Earings</p></td>
-</tr>
-<tr>
-<td><a href=""><img src="pics/necklace/Diamond8.jpg" alt="Mountain View" ></a><p class="jew">Necklace</p></td>
-<td><a href=""><img src="pics/rings/wedding/k9540264.jpg" alt="Mountain View" ></a><p class="jew">Rings</p></td>
-</tr>
-</table>
-</div>
+    <div id="body">
+        <div class="jewelry-grid">
+            <div class="jewelry-item">
+                <a href="Ankletes/Ankletes.php">
+                    <img src="pics/anklets/k14530991.jpg" alt="Anklets">
+                </a>
+                <p class="jew">Anklets</p>
+            </div>
+            <div class="jewelry-item">
+                <a href="">
+                    <img src="pics/bracelets/diamond-bracelets-7899035.jpg" alt="Bracelets">
+                </a>
+                <p class="jew">Bracelets</p>
+            </div>
+            <div class="jewelry-item">
+                <a href="">
+                    <img src="pics/earings/k16417032.jpg" alt="Earrings">
+                </a>
+                <p class="jew">Earrings</p>
+            </div>
+            <div class="jewelry-item">
+                <a href="">
+                    <img src="pics/necklace/Diamond8.jpg" alt="Necklace">
+                </a>
+                <p class="jew">Necklace</p>
+            </div>
+            <div class="jewelry-item">
+                <a href="">
+                    <img src="pics/rings/wedding/k9540264.jpg" alt="Rings">
+                </a>
+                <p class="jew">Rings</p>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <?php include_once("footer.php"); ?>
+    </footer>
 </body>
-<footer>
-<?php include_once("footer.php");?>
-</footer>
 </html>
