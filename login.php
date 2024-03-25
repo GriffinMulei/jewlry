68fdc0b00d67f01c8f76cecf8653a3ed15ecd1c1
Griffin <123077668+GriffinMulei@users.noreply.github.com>
2024-03-25 12:39:35 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,48 +1,45 @@
-
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<link href="css/loginReg.css" type="text/css" rel="Stylesheet"/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login to Jewelry for You</title>
+    <link href="css/loginReg.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 <div id="all">
-<h1>
-Login to Jewelery for u</h1>
-<nav id="nav">
-<ul>
-<li><a href="login.php">Login</a></li>
-<li><a href="adm.php">Admin</a></li>
-<li><a href="register.php">Register</a></li>
-</ul>
-</nav>
+    <h1>Login to Jewelry for You</h1>
+    <nav id="nav">
+        <ul>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="adm.php">Admin</a></li>
+            <li><a href="register.php">Register</a></li>
+        </ul>
+    </nav>
 <div id="web">
-<div id="logo">
-<img src="pics/jw.jpg"  "alt="Mountain View" >
-
-</div>
-<div id="login">
-<form id="loginform" method="post" action="account.php">
-<table>
-<tr>
-<td><label class="label">Username:</label></td>
-</tr>
-<tr>
-<td><input type="text" required name="username" class="lgn" placeholder="First Name"/></td>
-</tr>
-<tr>
-<td><label class="label">Password:</label></td>
-</tr>
-<tr>
-<td><input type="password" required name="password" class="lgn" placeholder="Password"/></td>
-</tr>
-<tr>
-<td><input type="submit" value="Login" class="login"></td>
-</tr>
-</table>
-</form>
-</div>
+    <div id="logo">
+        <!-- Corrected image tag -->
+        <img src="pics/jw.jpg" alt="Mountain View">
+    </div>
+    <div id="login">
+        <!-- Revised form with semantic HTML -->
+        <form id="loginform" method="post" action="account.php">
+            <div class="form-group">
+                <label for="username" class="label">Username:</label>
+                <input type="text" required id="username" name="username" class="lgn" placeholder="Username">
+            </div>
+            <div class="form-group">
+                <label for="password" class="label">Password:</label>
+                <input type="password" required id="password" name="password" class="lgn" placeholder="Password">
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Login" class="login">
+            </div>
+        </form>
+    </div>
 </div>
 <p>
-<marquee width="80%">Welcome back to Jewelery for u.</marquee></p>
+<marquee width="80%">Welcome back to Jewelery for u.</><p>Welcome back to Jewelry for You.</p>
 </div>
 </body>
 </html>
