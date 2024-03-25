8e91aa16a3e187f6909e09d8c7f948931cfc2f5c
Griffin <123077668+GriffinMulei@users.noreply.github.com>
2024-03-25 13:21:10 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,21 +1,30 @@
 <!DOCTYPE html>
-<html>
-<link href="css/css.css" rel="Stylesheet" type="text/css" />
-<footer> 
-<div id="footer">
-<div id="social">
-<tr>
-<td><a href="https://www.facebook.com/"><img src="pics/social/fb.jpg" alt="Mountain View" ></a></td>
-<td><a href="https://www.instagram.com/?hl=en"><img src="pics/social/insta.jpg" alt="Mountain View" ></a></td>
-<td><a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=6&ved=0ahUKEwiWtNDsiZ_JAhWGVRoKHaP3BI0QjBAIKzAF&url=https%3A%2F%2Fmanager.skype.com%2F&usg=AFQjCNGXNmZDqkFGfwRqBjsaFA6yossxKw&sig2=mobzXqia7a4ny_en1V6SLQ"><img src="pics/social/skype.jpg" alt="Mountain View" ></a></td>
-<td><a href="https://twitter.com/?lang=en"><img src="pics/social/twitter.png" alt="Mountain View" ></a></td>
-</tr>
-</div>
-<div id="footer1">
-Jewelery for you. Copyright &copy; 2015 </br>
-Registered trademark &reg; to Fellowh Wendoh
-</div>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Page Title Here</title>
+    <link href="css/css.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <!-- Your page content here -->
 
-</div>
-</footer>
+    <footer> 
+        <div id="footer">
+            <div id="social">
+                <!-- Use a list for social links for better semantics -->
+                <ul>
+                    <li><a href="https://www.facebook.com/"><img src="pics/social/fb.jpg" alt="Facebook" ></a></li>
+                    <li><a href="https://www.instagram.com/?hl=en"><img src="pics/social/insta.jpg" alt="Instagram" ></a></li>
+                    <li><a href="https://www.skype.com"><img src="pics/social/skype.jpg" alt="Skype" ></a></li>
+                    <li><a href="https://twitter.com/?lang=en"><img src="pics/social/twitter.png" alt="Twitter" ></a></li>
+                </ul>
+            </div>
+            <div id="footer1">
+                Jewelry for you. Copyright &copy; 2015<br>
+                Registered trademark &reg; to Fellowh Wendoh
+            </div>
+        </div>
+    </footer>
+</body>
 </html>
