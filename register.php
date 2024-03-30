1d6a9f57479e444a5c370fa5b42ae5aba775a230
Griffin <123077668+GriffinMulei@users.noreply.github.com>
2024-03-30 11:12:36 +0300

    Update register.php

--- register.php
+++ register.php
@@ -1,55 +1,68 @@
-
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<link href="css/loginReg.css" type="text/css" rel="Stylesheet"/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register | Jewelry for You</title>
+    <link rel="stylesheet" href="css/loginReg.css" type="text/css">
+    <!-- Consider hosting your CSS and other resources over HTTPS to avoid mixed content warnings -->
 </head>
 <body>
-<div>
-<!--Currency Converter widget by FreeCurrencyRates.com 
-
-<div id='gcw_mainF2d85tAG0' class='gcw_mainF2d85tAG0'></div>
-<script>function reloadF2d85tAG0(){ var sc = document.getElementById('scF2d85tAG0');if (sc) sc.parentNode.removeChild(sc);sc = document.createElement('script');sc.type = 'text/javascript';sc.charset = 'UTF-8';sc.async = true;sc.id='scF2d85tAG0';sc.src = 'http://freecurrencyrates.com/en/widget-horizontal-editable?iso=EURGBP&df=2&p=F2d85tAG0&v=s&source=fcr&width=400&width_title=200&firstrowvalue=1.00&thm=aaaaaa,ffffff,aaaaaa,cccccc,222222,C5C5c5,aaaaaa,212121,000000&title=Currency%20Converter&tzo=-180';var div = document.getElementById('gcw_mainF2d85tAG0');div.parentNode.insertBefore(sc, div);} reloadF2d85tAG0(); </script>
-<!-- put custom styles here: .gcw_mainF2d85tAG0{}, .gcw_headerF2d85tAG0{}, .gcw_ratesF2d85tAG0{}, .gcw_sourceF2d85tAG0{} -->
-<!--End of Currency Converter widget by FreeCurrencyRates.com -->
-</div>
-
 <div id="all">
-<h1>
-Register to Jewelery for u</h1>
-<nav id="nav">
-<ul>
-<li><a href="login.php">Login</a></li>
-<li><a href="adm.php">Admin</a></li>
-<li><a href="register.php">Register</a></li>
-</ul>
-</nav>
-<div id="web">
-<div id="logo">
-<img src="pics/jw.jpg"  "alt="Mountain View" >
+    <header>
+        <h1>Register to Jewelry for You</h1>
+        <nav id="nav">
+            <ul>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="adm.php">Admin</a></li>
+                <li><a href="register.php">Register</a></li>
+            </ul>
+        </nav>
+    </header>
 
-</div><div id="register">
-    <form id="loginform" method="post" action="account.php">
-        <div class="form-group">
-            <label for="username" class="label">Username:</label>
-            <input type="text" required name="username" id="username" class="lgn" placeholder="Username"/>
+    <main id="web">
+        <div id="logo">
+            <img src="pics/jw.jpg" alt="Jewelry Logo">
         </div>
-        <div class="form-group">
-            <label for="email" class="label">Email:</label>
-            <input type="email" required name="email" id="email" class="lgn" placeholder="email"/>
+
+        <div id="register">
+            <form id="registrationForm" method="post" action="account.php">
+                <div class="form-group">
+                    <label for="username">Username:</label>
+                    <input type="text" required name="username" id="username" class="lgn" placeholder="Username">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" required name="email" id="email" class="lgn" placeholder="Email">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input type="password" required name="password" id="password" class="lgn" placeholder="Password">
+                </div>
+                <div class="form-group">
+                    <input type="submit" value="Register" class="register">
+                </div>
+            </form>
         </div>
-        <div class="form-group">
-            <label for="password" class="label">Password:</label>
-            <input type="password" required name="password" id="password" class="lgn" placeholder="Password"/>
-        </div>
-        <div class="form-group">
-            <input type="submit" value="Register" class="register">
-        </div>
-    </form>
-</div>
-</div>
-</div>
-<p>
-<marquee width="80%">Feel free to join Jewelery for u.</marquee></p>
+    </main>
+
+    <!-- Currency Converter Widget, ensure HTTPS is used for external resources -->
+    <div>
+        <script>
+            function loadCurrencyConverter() {
+                var script = document.createElement('script');
+                script.type = 'text/javascript';
+                script.src = 'https://freecurrencyrates.com/en/widget-horizontal-editable?iso=EURGBPUSD&df=2&p=F2d85tAG0&v=s&source=fcr&width=400&width_title=200&firstrowvalue=1.00&thm=aaaaaa,ffffff,aaaaaa,cccccc,222222,C5C5c5,aaaaaa,212121,000000&title=Currency%20Converter&tzo=-180';
+                document.body.appendChild(script);
+            }
+            window.onload = loadCurrencyConverter;
+        </script>
+    </div>
+
+    <!-- Replacing <marquee> with a CSS-based animation or static text -->
+    <footer>
+        <p>Feel free to join Jewelry for You.</p>
+    </footer>
 </div>
 </body>
 </html>
